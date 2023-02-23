66250f560c6b754fecd4d1e759a1864092f8abbd
Muhammad Usman <musman0741@gmail.com>
2023-02-23 19:39:54 +0500

    [master] new custom hook added

---
+++ src/utils/useFocus.ts
@@ -0,0 +1,12 @@
+// React Dependencies
+import { useRef, useEffect } from "react";
+
+export const useFocus = () => {
+  const ref = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    ref.current?.focus();
+  }, []);
+
+  return ref;
+};
